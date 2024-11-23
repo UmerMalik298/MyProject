334f549831492dd45032176df08de2dcdd464ac9
Umer Malik <144059239+UmerMalik298@users.noreply.github.com>
2024-11-23 22:30:40 +0500

    Add files via upload

---
+++ SendGridEmailSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class SendGridEmailSender : IEmailSender
+{
+    private readonly IConfiguration _configuration;
+
+    public SendGridEmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string message)
+    {
+        var apiKey = _configuration["SendGrid:ApiKey"];
+        var client = new SendGridClient(apiKey);
+
+        var from = new EmailAddress(_configuration["SendGrid:SenderEmail"], _configuration["SendGrid:SenderName"]);
+        var to = new EmailAddress(email);
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent: message, htmlContent: message);
+
+        await client.SendEmailAsync(msg);
+    }
+}
